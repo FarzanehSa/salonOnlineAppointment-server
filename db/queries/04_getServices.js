fb553c03806ae68956b7c31e7c06462be662bc9d
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-10-30 16:07:02 -0700

    correct query for get services

--- db/queries/04_getServices.js
+++ db/queries/04_getServices.js
@@ -3,7 +3,7 @@ const db = require('../connection');
 const getServices = () => {
   return db.query(`
   SELECT services.id, services.name as service, services.price, services.description,
-    service_groups.name as group
+  service_groups.id as groupid, service_groups.name as group
     FROM services
     JOIN service_groups ON services.service_group_id = service_groups.id
     ORDER BY service_groups.id, services.id;`
