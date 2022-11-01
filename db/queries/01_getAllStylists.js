5e93185998e477433315b975a5641e3f1e8ddc8b
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-11-01 15:11:56 -0700

    add skills array to each stylist info

--- db/queries/01_getAllStylists.js
+++ db/queries/01_getAllStylists.js
@@ -3,9 +3,11 @@ const db = require('../connection');
 const getAllStylists = () => {
   return db.query(`
   SELECT stylists.id as id, stylists.name as name, image, bio,
-    stylist_levels.name as level
+    stylist_levels.name as level, array_agg(stylist_skills.service_group_id) as skills
     FROM stylists
     JOIN stylist_levels ON stylists.stylist_level_id = stylist_levels.id
+    JOIN stylist_skills ON stylists.id = stylist_skills.stylist_id
+    GROUP BY stylists.id, stylist_levels.name
     ORDER BY stylists.id;`
   )
     .then(data => {
