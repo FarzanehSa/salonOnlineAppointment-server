0df9c698ff13649a37899c032a98420b2436d3ec
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-11-01 16:09:17 -0700

    left join in group query to show empty service groups

--- db/queries/03_getServiceGroups.js
+++ db/queries/03_getServiceGroups.js
@@ -5,7 +5,7 @@ const getServiceGroups = () => {
   SELECT service_groups.id, service_groups.name as group,
     array_agg(stylist_skills.stylist_id) as stylists
     FROM service_groups
-    JOIN stylist_skills ON stylist_skills.service_group_id = service_groups.id
+    LEFT JOIN stylist_skills ON stylist_skills.service_group_id = service_groups.id
     GROUP By service_groups.id
     ORDER BY service_groups.id;`
   )
